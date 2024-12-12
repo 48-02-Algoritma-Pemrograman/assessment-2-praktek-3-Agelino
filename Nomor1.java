d4ddc712ea4d49689d0d0302a3c042b564c058d3
Agelino <144404858+Agelino@users.noreply.github.com>
2024-12-12 14:29:26 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
 
-public class Nomor1 {
+public class Nomor1{
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int n = scanner.nextInt();
 
-        System.out.print("Masukkan alas segitiga: ");
-        double alas = input.nextDouble();
-        System.out.print("Masukkan tinggi segitiga: ");
-        double tinggi = input.nextDouble();
-        double luas = hitungLuasSegitiga(alas, tinggi);
-        System.out.println("Luas segitiga: " + luas);
-
-    
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz ");
             } else if (i % 3 == 0) {
@@ -25,13 +18,5 @@ public class Nomor1 {
                 System.out.print(i + " ");
             }
         }
-        System.out.println();
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length);
-    }
-
-    
-    public static double hitungLuasSegitiga(double alas, double tinggi) {
-        return 0.5 * alas * tinggi;
     }
 }

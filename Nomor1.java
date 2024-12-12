1da43da92ccadeff3f83a76d7ae283530dbbe578
Agelino <144404858+Agelino@users.noreply.github.com>
2024-12-12 13:57:39 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,37 @@
 import java.util.Scanner;
 
 public class Nomor1 {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
+        
 
-        /* Pekerjaan anda berakhir sini */
+        System.out.print("Masukkan alas segitiga: ");
+        double alas = input.nextDouble();
+        System.out.print("Masukkan tinggi segitiga: ");
+        double tinggi = input.nextDouble();
+        double luas = hitungLuasSegitiga(alas, tinggi);
+        System.out.println("Luas segitiga: " + luas);
 
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
-    }
     
+        for (int i = 0; i <= N; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.println(Nomor1.class.getDeclaredMethods().length);
+    }
+
+    
+    public static double hitungLuasSegitiga(double alas, double tinggi) {
+        return 0.5 * alas * tinggi;
+    }
 }
